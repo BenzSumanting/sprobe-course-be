f2cdc595e50bfe8387c1b323fc514c1c5c662c2f
Benz-mtechlab <benzsuma@mtechlabs.dev>
2025-08-29 15:34:34 +0800

    fix: Course model from Due date to credits

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -11,7 +11,7 @@ class Course extends Model
 {
     use HasFactory, HasUlid, SoftDeletes;
 
-    protected $fillable = ['title', 'description', 'due_date'];
+    protected $fillable = ['title', 'description', 'credits'];
 
     public function students()
     {
