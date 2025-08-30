33e04a749799bbcfb31cf9b23ebe75b659a9df84
Benz-mtechlab <benzsuma@mtechlabs.dev>
2025-08-30 17:31:27 +0800

    feat: add assignment, student assignments and student score

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssignmentController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\StudentController;
@@ -10,4 +11,8 @@ Route::post('login', LoginController::class);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('student', StudentController::class);
     Route::apiResource('course', CourseController::class);
+    Route::apiResource('assignment', AssignmentController::class);
+
+    Route::get('student/{id}/assignment', [StudentController::class, 'studentAssignment']);
+    Route::post('student/{id}/score', [StudentController::class, 'studentScore']);
 });
