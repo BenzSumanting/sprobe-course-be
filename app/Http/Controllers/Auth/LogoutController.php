b537792775d687b2b2f19eb16a2842903817de16
Benz-mtechlab <benzsuma@mtechlabs.dev>
2025-08-31 13:33:35 +0800

    feat: implement logout

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\ApiResponse\ApiResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
     //
+    public function __invoke(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return ApiResponse::success(null, 'Success', 204);
+    }
 }
